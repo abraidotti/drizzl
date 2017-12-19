ca758807d49c3430644cd89edf4777cdba2b8862
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-19 15:51:56 -0500

    add amazing hacky render

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var https = require("https");
+var request = require('request');
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://drizzl:drizzltest@ds161146.mlab.com:61146/forecasts';
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 
 app.use('*/css',express.static('public/css'));
 app.use('*/js',express.static('public/js'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/forecast', function(req, res){
-  res.render('forecast', { qs: req.query });
+app.post('/', function(req, res){
+  request(`https://api.darksky.net/forecast/8c3c8dc972b787fa631b37e0cf3da0d2/${req.body.locationstring}?exclude=minutely,hourly,daily,alerts,flags`, function (error, response, body) {
+  // Print the error if one occurred
+  console.log('error:', error);
+  // Print the response status code if a response was received
+  console.log('statusCode:', response && response.statusCode);
+  // Print the api's response.
+  console.log('body:', JSON.parse(body));
+  res.render('forecast', { body });
+  });
 });
 
-app.get('/forecast/:id', function(req, res){
-  res.render('forecast', {id: req.params.id}) + '.';
-})
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(PORT, function(){
+		console.log('Running on: ' + PORT);
+});
---
+++ public/js/locate.js
@@ -0,0 +1,121 @@
+var forecast, forecastLoader, crd, userLat, userLong;
+
+const navigatorLocateButton = document.querySelector("#locate-with-browser");
+const locationInput = document.querySelector("#location-input");
+const locationButton = document.querySelector("#location-button");
+// navigator geolocation options
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+// if user hits the "get my location button":
+navigatorLocateButton.addEventListener('click', function(){
+  showLoader();
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+  // if successful grab, set coordinates
+  function success(pos) {
+    crd = pos.coords;
+    userLat = crd.latitude;
+    userLng = crd.longitude;
+
+    // tell the user what's found
+    console.log("Location found.");
+    console.log("Your current position is:");
+    console.log(`Latitude : ${userLat}`);
+    console.log(`Longitude: ${userLng}`);
+    readyLatLng(userLat, userLng);
+  }
+  // log error messages
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+});
+
+// if user submits a location:
+locationButton.addEventListener("click", function(event) {
+  // prevent page reload on form submission
+  event.preventDefault();
+
+  var submittedLocation = locationInput.value;
+
+  showLoader();
+  // geocode the user's location
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ address: submittedLocation }, function(results,status){
+    if (status === "OK") {
+      var geoLocation = results[0].geometry.location
+        .toString()
+        .replace(/\s/g, "");
+      userLat = results[0].geometry.location.lat();
+      userLng = results[0].geometry.location.lng();
+
+      // tell the user what's found
+      console.log("Location found.");
+      console.log("Your current position is:");
+      console.log(`Latitude : ${userLat}`);
+      console.log(`Longitude: ${userLng}`);
+
+      readyLatLng(userLat, userLng);
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    };
+  }); // end of geocode block
+}); // end of click function
+
+function showLoader(){
+  document.querySelector("#container").remove();
+  forecastLoader = document.createElement("DIV");
+  forecastLoader.setAttribute("id", "forecast-loader");
+  document.body.appendChild(forecastLoader);
+};
+
+// get a forecast!
+function getForecast(lat, lng) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET",`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8c3c8dc972b787fa631b37e0cf3da0d2/${lat},${lng}?exclude=minutely,hourly,daily,alerts,flags`);
+  console.log("OPENED", xhr.status);
+
+  xhr.onload = function() {
+    console.log("LOADING", xhr.status);
+    if (xhr.status === 200) {
+      forecast = JSON.parse(xhr.responseText);
+      console.log(forecast);
+    } else {
+      alert("Request failed.  Returned status of " + xhr.status);
+    }
+  };
+  xhr.send();
+}
+
+function readyLatLng(lat, lng){
+  // kill the loading animation
+  forecastLoader.remove();
+
+  var readyDiv = document.createElement("DIV");
+  readyDiv.setAttribute("id", "container");
+  document.body.appendChild(readyDiv);
+
+  var readyDivHeader = document.createElement("H1");
+  readyDivHeader.textContent = "Location ready.";
+  readyDiv.appendChild(readyDivHeader);
+
+  var readyForm = document.createElement("FORM");
+  readyForm.setAttribute("method", "post");
+  readyForm.setAttribute("action", "/");
+  readyDiv.appendChild(readyForm);
+
+  var readyFormText = document.createElement("input"); //input element, text
+  readyFormText.setAttribute('type',"text");
+  readyFormText.setAttribute('name',"locationstring");
+  readyFormText.setAttribute('size',"30");
+  readyFormText.setAttribute('value',`${lat},${lng}`);
+  readyForm.appendChild(readyFormText);
+
+  var readyFormButton = document.createElement("input"); //input element, Submit button
+  readyFormButton.setAttribute('type',"submit");
+  readyFormButton.setAttribute('value',"Submit");
+  readyForm.appendChild(readyFormButton);
+}
