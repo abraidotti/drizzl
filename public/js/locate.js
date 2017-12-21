5682d266388a404916b1291411defa24b7fa8d61
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-21 09:50:08 -0500

    add canvas loader spinner, ditch css loader spinner

--- public/js/locate.js
+++ public/js/locate.js
@@ -67,9 +67,38 @@ locationButton.addEventListener("click", function(event) {
 
 function showLoader(){
   document.querySelector("#container").remove();
-  forecastLoader = document.createElement("DIV");
-  forecastLoader.setAttribute("id", "forecast-loader");
+  forecastLoader = document.createElement("CANVAS");
+  forecastLoader.setAttribute("id", "spinner");
+  forecastLoader.setAttribute("height", "window.innerHeight");
+  forecastLoader.setAttribute("width", "window.innerWidth");
   document.body.appendChild(forecastLoader);
+
+  var canvas = document.getElementById('spinner');
+  var context = canvas.getContext('2d');
+  var start = new Date();
+  var lines = 16,
+    cW = context.canvas.width,
+    cH = context.canvas.height;
+
+  var draw = function() {
+    var rotation = parseInt(((new Date() - start) / 1000) * lines) / lines;
+    context.save();
+    context.clearRect(0, 0, cW, cH);
+    context.translate(cW / 2, cH / 2);
+    context.rotate(Math.PI * 2 * rotation);
+    for (var i = 0; i < lines; i++) {
+
+        context.beginPath();
+        context.rotate(Math.PI * 2 / lines);
+        context.moveTo(cW / 10, 0);
+        context.lineTo(cW / 4, 0);
+        context.lineWidth = cW / 30;
+        context.strokeStyle = "rgba(0, 0, 0," + i / lines + ")";
+        context.stroke();
+    }
+    context.restore();
+};
+window.setInterval(draw, 1000 / 40);
 };
 
 // get a forecast!
